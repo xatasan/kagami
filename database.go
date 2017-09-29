e01cef6cab3a0f8802a823ed35984a5949a83a9f
Xatasan <xatasan@firemail.cc>
2017-09-30 01:04:54 +0200

    Added full text search support for posts

--- database.go
+++ database.go
@@ -78,6 +78,32 @@ func setupdb(dbfile string) (err error) {
         INSERT INTO links (post, file, ord) VALUES (?, ?, ?)
         `)
 
+	// full-text search
+	if _, err = db.Exec(`
+        CREATE VIRTUAL TABLE IF NOT EXISTS search
+        USING fts5(
+            content=posts,
+            content_rowid=postno,
+            comment,
+            subject
+        );
+        CREATE TRIGGER IF NOT EXISTS post_ai AFTER INSERT ON posts BEGIN
+            INSERT INTO search(rowid, comment, subject)
+                   VALUES (new.postno, new.comment, new.subject);
+        END;
+        CREATE TRIGGER IF NOT EXISTS tbl_ad AFTER DELETE ON posts BEGIN
+            INSERT INTO search(search, rowid, comment, subject)
+                   VALUES('delete', old.postno, old.comment, old.subject);
+        END;
+        CREATE TRIGGER IF NOT EXISTS tbl_au AFTER UPDATE ON posts BEGIN
+            INSERT INTO search(search, rowid, comment, subject)
+                   VALUES('delete', old.postno, old.comment, old.subject);
+            INSERT INTO search(rowid, comment, subject)
+                   VALUES (new.postno, new.comment, new.subject);
+        END;`); err != nil {
+		return
+	}
+
 	return
 }
 
